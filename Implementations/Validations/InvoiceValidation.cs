a6d1fd600b0a8edcb39edf34fcaee9d6a657c354
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2022-11-26 16:28:35 +0300

    add invoice validation

---
+++ Implementations/Validations/InvoiceValidation.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Core.Models;
+using Domain.Core.Models.Orders;
+using Domain.Core.Models.Roles;
+using Services.Interfaces.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations.Validations
+{
+	public class InvoiceValidation : IInvoiceValidation
+	{
+		private Invoice _invoice;
+
+		public InvoiceValidation(Invoice invoice)
+		{
+			_invoice = invoice;
+		}
+
+		public bool ContainClient(Client client)
+		{
+			if (client == null)
+				return false;
+
+		}
+
+		public bool ContainEmployee(Employee employee)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ContainOrder(Order order)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Services.Interfaces/Validations/IInvoiceValidation.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Core.Models.Orders;
+using Domain.Core.Models.Roles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces.Validations
+{
+	public interface IInvoiceValidation : IIsNotNull
+	{
+		public bool ContainOrder(Order order);
+		public bool ContainClient(Client client);
+		public bool ContainEmployee(Employee employee);
+
+	}
+}
